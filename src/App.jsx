b7cc985ceb31c73a040cfe7bee50a7911c244a74
Fonsitax <mynameisfonja@gmail.com>
2024-07-21 11:59:26 +0200

    router created

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='text-xl font-mono text bg-red-300'>Hello from App.jsx. I am Tailwind CSS. I am not functioning!</h1>
+        
+      </div>
+      
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world from Main.jsx
+    </div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+       <RouterProvider router={router} />
+)
+
